d405b6bfa68a55828fd0701ab5b962a553515720
Jimmy Tidey <jimmytidey@gmail.com>
2015-06-13 00:51:38 +0100

    bar graph responding

--- app/scripts/controllers/tax/incomeTaxCtrl.js
+++ app/scripts/controllers/tax/incomeTaxCtrl.js
@@ -11,21 +11,26 @@ angular.module('nationlBalanceApp')
   .controller('incomeTaxCtrl', function ($scope, $location) {
       
     $scope.incomeTax = {};
-    $scope.tax_totals.push({name: 'Income Tax', value: 0})
+    $scope.tax_totals.push({name: 'Income Tax', value: 120})
 
     //set default state for main view
     $scope.incomeTax.var1 = 200; 
 
-
     //check if anything is set in the URL 
     $scope.helpers.getUrlParams($scope, $location);
 
-    $scope.calculateIncomeTax = function() { 
-      
 
-      $scope.helpers.renderTax();
+    $scope.calculateIncomeTax = function() {
+
+        $.each($scope.tax_totals, function(key,val) { 
+            if(val.name==='Income Tax') { 
+                val.value =  $scope.incomeTax.var1;
+            }
+        });
+    
+        $scope.helpers.renderTax($scope);
     };
 
-    $scope.helpers.renderTax();
+    $scope.calculateIncomeTax();
 });
 
--- app/scripts/renderTaxHelper.js
+++ app/scripts/renderTaxHelper.js
@@ -1,3 +1,16 @@
-nationlBalanceApp.helpers.renderTax = function(){ 
-	console.log('render tax');
+nationlBalanceApp.helpers.renderTax = function($scope){ 
+	var total_tax = 0; 
+
+	var position = $('.tax_bar_container').height()/2; 
+	$('.tax_bar').css({bottom: -position});
+
+	$.each($scope.tax_totals, function(key, val) { 
+		total_tax += val.value; 
+	});
+
+
+	$('.tax_bar').css({height: total_tax});
+	
+	console.log('called');
+	
 };
